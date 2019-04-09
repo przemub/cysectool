2edad71153fb38c007ef38d1ac5fdf4a610fdc91
Przemysław Buczkowski <przemub@przemub.pl>
2019-04-09 20:01:53 +0100

    add edit.html

--- my_static/js/edit.js
+++ my_static/js/edit.js
@@ -1,42 +1,119 @@
-const clients = [
-    {"Name": "Otto Clay", "Age": 25, "Country": 1, "Address": "Ap #897-1459 Quam Avenue", "Married": false},
-    {"Name": "Connor Johnston", "Age": 45, "Country": 2, "Address": "Ap #370-4647 Dis Av.", "Married": true},
-    {"Name": "Lacey Hess", "Age": 29, "Country": 3, "Address": "Ap #365-8835 Integer St.", "Married": false},
-    {"Name": "Timothy Henson", "Age": 56, "Country": 1, "Address": "911-5143 Luctus Ave", "Married": true},
-    {
-        "Name": "Ramona Benton",
-        "Age": 32,
-        "Country": 3,
-        "Address": "Ap #614-689 Vehicula Street",
-        "Married": false
+const verticesGrid = $("#verticesGrid");
+const groupsGrid = $("#groupsGrid");
+const levelsGrid = $("#levelsGrid");
+const edgesGrid = $("#edgesGrid");
+
+function updateGrid(grid, item) {
+    const groups = groupsGrid.jsGrid("option", "data");
+    console.log(groups);
+
+    let new_levels = [];
+    for (const group of groups) {
+        console.log(group);
+        for (let i = 1; i <= group.levels; i++)
+            new_levels.push({
+                'gid': group.id,
+                'level': i.toString(),
+                'name': group.id+" "+i.toString(),
+                'cost': 1,
+                'ind_cost': 1,
+                'flow': 1
+            });
     }
-];
 
-const countries = [
-    {Name: "", Id: 0},
-    {Name: "United States", Id: 1},
-    {Name: "Canada", Id: 2},
-    {Name: "United Kingdom", Id: 3}
-];
+    const levels = levelsGrid.jsGrid("option", "data");
+    // TODO: Do it more efficiently
+    for (const level of levels) {
+        for (const i in new_levels)
+            if (new_levels[i]['gid'] === level['gid'] && new_levels[i]['level'] === level['level'])
+                new_levels[i] = level;
+    }
 
-$("#jsGrid").jsGrid({
-            width: "100%",
-            height: "400px",
+    levelsGrid.jsGrid("option", "data", new_levels);
+    console.log(new_levels);
+}
 
-            inserting: true,
-            editing: true,
-            sorting: true,
-            paging: true,
+verticesGrid.jsGrid({
+    width: "100%",
+    height: "400px",
 
-            data: clients,
+    inserting: true,
+    editing: true,
+    sorting: true,
+    paging: false,
 
-            fields: [
-                { name: "Name", type: "text", width: 150, validate: "required" },
-                { name: "Age", type: "number", width: 50 },
-                { name: "Address", type: "text", width: 200 },
-                { name: "Country", type: "select", items: countries, valueField: "Id", textField: "Name" },
-                { name: "Married", type: "checkbox", title: "Is Married", sorting: false },
-                { type: "control" }
-            ]
-        });
+    data: vertices,
 
+    fields: [
+        { name: "id", title: "ID", type: "number", width: 50, validate: "required" },
+        { name: "name", title: "Name", type: "text", width: 150, validate: "required" },
+        { type: "control" }
+    ]
+});
+
+
+groupsGrid.jsGrid({
+    width: "100%",
+    height: "400px",
+
+    inserting: true,
+    editing: true,
+    sorting: true,
+    paging: false,
+
+    data: groups,
+
+    onItemInserted: updateGrid,
+    onItemUpdated: updateGrid,
+
+    fields: [
+        { name: "id", title: "ID", type: "text", width: 50, validate: "required" },
+        { name: "name", title: "Name", type: "text", width: 150, validate: "required" },
+        { name: "levels", title: "Number of levels", type: "number", width: 50, validate: "required" },
+        { type: "control" }
+    ]
+});
+
+
+levelsGrid.jsGrid({
+    width: "100%",
+    height: "400px",
+
+    inserting: false,
+    editing: true,
+    sorting: true,
+    paging: false,
+
+    data: levels;
+
+    fields: [
+        { name: "gid", title: "Group ID", type: "text", width: 50, readOnly: true },
+        { name: "level", title: "Level", type: "text", width: 50, readOnly: true },
+        { name: "name", title: "Name", type: "text", width: 150, validate: "required" },
+        { name: "cost", title: "Cost", type: "number", width: 50, validate: "required" },
+        { name: "ind_cost", title: "Indirect cost", type: "number", width: 50, validate: "required" },
+        { name: "flow", title: "Flow", type: "number", width: 50, validate: "required" },
+        { type: "control", deleteButton: false }
+    ]
+});
+
+
+edgesGrid.jsGrid({
+    width: "100%",
+    height: "400px",
+
+    inserting: true,
+    editing: true,
+    sorting: true,
+    paging: false,
+
+    data: edges,
+
+    fields: [
+        {name: "source", title: "Source", type: "number", width: 50, validate: "required"},
+        {name: "target", title: "Target", type: "number", width: 50, validate: "required"},
+        {name: "name", title: "Vulnerability name", type: "text", width: 150, validate: "required"},
+        {name: "controls", title: "Valid controls", type: "text", width: 100, validate: "required"},
+        {type: "control"}
+    ]
+});
--- my_static/js/main.js
+++ my_static/js/main.js
@@ -2,9 +2,11 @@ let $ = (arg) => document.querySelector(arg);
 
 // Avoids bug #8065 in Bokeh
 async function arrow() {
+    // noinspection JSJQueryEfficiency
     while (!$('div.bk-tooltip-arrow'))
         await new Promise(r => setTimeout(r, 250));
 
+    // noinspection JSJQueryEfficiency
     let arrow = $('div.bk-tooltip-arrow');
     if (arrow && arrow.parentNode)
         arrow.parentNode.removeChild(arrow);
