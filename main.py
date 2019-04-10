b1e9078f41e4f925e822ea763418a2592850c04a
Przemysław Buczkowski <przemub@przemub.pl>
2019-04-10 12:15:42 +0200

    allow remote access

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ from src import visualiser, api, edit
 server = Server({'/visualiser': Application(FunctionHandler(visualiser.main))},
                 extra_patterns=[('/api', api.ApiHandler),
                                 ('/edit', edit.EditHandler),
-                                (r'/my_static/(.*)', tornado.web.StaticFileHandler, {"path": "my_static/"})])
+                                (r'/my_static/(.*)', tornado.web.StaticFileHandler, {"path": "my_static/"})],
+                allow_websocket_origin=["hegel.eecs.qmul.ac.uk:5006", "attackgraphs.eecs.qmul.ac.uk", "localhost:5006"])
 server.start()
 
 if __name__ == '__main__':
