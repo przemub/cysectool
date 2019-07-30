8ce03f7b42a2eb043b5c42c1840214573e490ead
Przemysław Buczkowski <przemub@przemub.pl>
2019-07-30 17:01:33 +0100

    update frequency (finished) and detectability (in progress) analysis

--- doc/automatic_scanners/wapiti_run.sh
+++ doc/automatic_scanners/wapiti_run.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
 # INFO: You need to disable compulsory authentication in DVWA for it to run.
 
-wapiti -u "http://localhost:8000/" -x http://localhost:8000/login.php -m "backup,blindsql,buster,crlf,delay,exec,file,htaccess,methods,nikto,permanentxss,shellshock,sql,ssrf,xss" -f txt --color
+if [ -z $1 ]
+then
+	echo "Usage: $0 security_level (low/medium/high/impossible)"
+	exit 1
+fi
+
+wapiti-getcookie -u http://localhost:8000/ -c cookie.json > /dev/null
+sed -i "s/low/$1/g" cookie.json
+
+wapiti -u "http://localhost:8000/" -x http://localhost:8000/login.php -x http://localhost:8000/logout.php -x http://localhost:8000/security.php -m "backup,blindsql,crlf,delay,exec,file,htaccess,methods,nikto,permanentxss,shellshock,sql,ssrf,xss" -f txt --color --flush-session -c cookie.json --scope domain 
+# rm cookie.json
+
